cba094e4fb40c70c59bb5f3f91aa30939c2c2a87
ahmed ekramy <ahmed@ekramy>
2025-08-30 17:07:55 +0300

    finish section

--- Node_Farm/index.js
+++ Node_Farm/index.js
@@ -1,19 +1,35 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require("slugify");
+
+console.log(slugify("Fresh Avocado", { lower: true }));
 
 const data = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
 const product = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
 const card = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
 
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+const manualy = dataObj.map((el) => el.productName.toLowerCase().split(" ").join("-"));
+console.log(manualy);
 
-  if (pathName === "/" || pathName === "overview") {
-    res.end(overview);
-  } else if (pathName === "/product") {
-    res.end("this is product");
-  } else if (pathName === "/api") {
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+
+  if (pathname === "/" || pathname === "overview") {
+    const output = dataObj.map((el) => replaceTemplate(card, el)).join("");
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    const finalOutput = overview.replace("{%cards%}", output);
+    res.end(finalOutput);
+  } else if (pathname === "/product") {
+    res.end(replaceTemplate(product, dataObj[query.id]));
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
