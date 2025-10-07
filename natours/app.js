d9b640ac8d3083f6b3888177bc66528d6fe639e3
ahmed ekramy <ahmed@ekramy>
2025-10-07 04:11:48 +0300

    start learning methods

--- natours/app.js
+++ natours/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 const fs = require('fs');
 
-// app.get('/', (req, res) => {
-//   //   res.status(200).send('Hello form server side');
-//   res.status(404).json({ message: 'Hello form server side' });
-// });
-
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// ROUTE HANDLERS
+
+// GET all tours
 app.get('/api/v2/tours', (req, res) => {
   res
     .json({
@@ -19,6 +17,59 @@ app.get('/api/v2/tours', (req, res) => {
       data: {
         tours,
       },
+    })
+    .status(200);
+});
+
+// GET spacific tour by ID
+app.get('/api/v2/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // convert string to number
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res
+    .json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    })
+    .status(200);
+});
+
+// POST a new tour
+app.post('/api/v2/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// PATCH update tour by id
+
+app.patch('/api/v2/tours/:id', (req, res) => {
+  const id = req.params.id;
+  res
+    .json({
+      message: 'success',
+      updatet: true,
+      id,
     })
     .status(200);
 });
